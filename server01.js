6cf9181d6ed16d39f4bc3a8eb48bbc16a1c27c27
Knielsza <piontek.milosz@gmail.com>
2023-05-10 12:01:21 +0200

    ok

--- server01.js
+++ server01.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const PORT = 3000;
 
+// const newDirPath = require("string_decoder")
 const fs = require("fs")
 const path = require("path")
 
@@ -9,69 +10,15 @@ const path = require("path")
 //     res.send("dane html odesłane z serwera do przeglądarki")
 //     console.log("kotleciki")
 // })
-
-app.get('/', function (req, res) {
-  // fs.readFile("./files/file01.txt", (err, data) => {
-  //   if (err) throw err
-  //   console.log(data.toString());
-  // }) 
-  //   //Zczytywanie informacji z pliku
-  //-----------------------------------------------------------------------
-
-  // fs.readFile("./files/file01.txt", "utf-8", (err, data) => {
-  //   if (err) throw err
-  //   console.log(data.toString());
-  // })
-  //   //Zczytywanie informacji z pliku z kodowaniem
-  //-----------------------------------------------------------------------
-  const filepath2 = path.join(__dirname, "files", "file02.txt")
-  const filepath = path.join(__dirname, "files", "file01.txt")
-
-  // fs.writeFile(filepath2, "tekst do wpisania", (err) => {
-  //   if (err) throw err
-  //   console.log("plik nadpisany");
-  // })
-  //   //Nadpisywanie/Zapisywanie pliku
-  //-----------------------------------------------------------------------
-
-  // fs.appendFile(filepath2, "\n\ntekst do dopisania", (err) => {
-  //   if (err) throw err
-  //   console.log("plik utworzony");
-  // })
-  //   //Dopisywanie do pliku
-  //-----------------------------------------------------------------------
-
-  //   fs.unlink(filepath, (err) => {
-  //     if (err) throw err
-  //     console.log("czas 1: " + new Date().getMilliseconds());
-  //   })
-  //   //Usuwanie pliku
-  //-----------------------------------------------------------------------
-
-  // if (fs.existsSync(filepath)) {
-  //   console.log("plik istnieje");
-  // } else {
-  //   console.log("plik nie istnieje");
-  // }
-  // //Sprawdzanie czy plik istnieje
-
-  const filepath3 = path.join(__dirname, "files", "file03.txt")
-  const filepath4 = path.join(__dirname, "files", "file04.txt")
-
-  fs.writeFile(filepath3, "tekst do zapisania", (err) => {
-    if (err) throw err
-    console.log("plik utworzony - czas 1: " + new Date().getMilliseconds());
-
-    fs.appendFile(filepath3, "\n\ntekst do dopisania", (err) => {
-      if (err) throw err
-      console.log("plik zmodyfikowany - czas 2: " + new Date().getMilliseconds());
-
-    })
-  })
-
-
+fs.mkdir(newDirPath, (err) => {
+  if (err) throw err
+  else {
+    console.log("katalog powstał")
+  }
 })
-
+app.get('/', function (req, res) {
+  res.render('index.hbs')
+})
 
 app.listen(PORT, function () {
   console.log("start serwera na porcie " + PORT)
